1c51e9f34a0778ed5777ec03ad2d1cc9d2534474
bluy-016 <abhinav0000016@gmail.com>
2026-05-13 11:49:46 +0530

    added built-in function and fixed the process waiting bug

--- abvsh.c
+++ abvsh.c
@@ -11,11 +11,16 @@
 
 //day - 1 phrasing the input
 //day - 2 fork and execute 
+//day - 3 fixing the termination bug 
+//day - 4 adding the built in commands
+
 void loop();
 char **parser(char *line);
 char *readline();
 int executer(char **tokens);
-
+int abvsh_cd(char **tokens);
+int abvsh_help(char **tokens);
+int abvsh_exit(char **tokens);
 int main(){
 	loop();
 
@@ -28,14 +33,14 @@ void loop(){
 	char *usr_name = getenv("USER");
 	char **tokens;
 	char *line;
-  printf("(%s)>",usr_name);
 	do{
+    printf("(%s)>",usr_name);
+    fflush(stdout);
 		line = readline();
 		tokens = parser(line);
     status = executer(tokens);
+    free(line);
 		free(tokens);
-		free(line);
-    printf("(%s)>",usr_name);
 	}while(status);
 }
 
@@ -98,24 +103,76 @@ char **parser(char *line){
   }
 }
 
-int executer(char **tokens){
+int create(char **tokens){
   pid_t id = fork();
   int stat;
-  if(id == -1){
+  if(id == -1){ // fork error
     perror("abvsh");
   }
-  else if (id == 0){ // this is for child process 
+  else if (id == 0){ // child process
     int exe = execvp(tokens[0],tokens);
     if(exe == -1){
       perror("abvsh");
       exit(EXIT_FAILURE);//to kill the child process not the shell
     }
-    else{
-      do{
-        waitpid(id,&stat,WUNTRACED);//it will wait until the child process is terminated by a signal or until it is completed normally (by return 0 or exit in its code )
-      }while(!WIFEXITED(stat) && !WIFSIGNALED(stat));//wifexited for normal termination & wifsignaled for termination by signals like ctrl + c and other keys
+    exit(EXIT_FAILURE);//to kill the child process not the shell
+  }
+  else{ // parent process 
+    do{
+      waitpid(id,&stat,WUNTRACED);//it will wait until the child process is terminated by a signal or until it is completed normally (by return 0 or exit in its code )
+    }while(!WIFEXITED(stat) && !WIFSIGNALED(stat));//wifexited for normal termination & wifsignaled for termination by signals like ctrl + c and other keys
+  }
+  return 1;
+}
+
+char *builtin_func[]= {
+  "cd","help","exit"
+};
+
+int no_of_builtin(){
+  return (sizeof(builtin_func) / sizeof(char*));
+}
+
+//name the builtin fuction with shell name before to avoid fuction name conflict 
+int abvsh_cd(char **tokens){
+  if(tokens[1] == NULL){
+    fprintf(stderr,"abvsh: expected argument to \"cd\" \n");
+  }
+  else{
+    if(chdir(tokens[1]) != 0){
+      perror("abvsh");
     }
   }
   return 1;
+}
+
+int abvsh_help(char **tokens){
+  printf("\n");
+  printf("Abhinav's Basic shell : abvsh \n");
+  printf("type the program nmae and the argumnets and press enter\n");
+  printf("The Builtin fucntions are:\n");
+  for (int i = 0 ; i < no_of_builtin() ; i++){
+    printf("\t%s\n",builtin_func[i]);
+  }
+  printf("\n");
+  return 1;
+}
+
+int abvsh_exit(char **tokens){
+  return 0; 
+}
+
+int (*farr[])(char **) = { abvsh_cd , abvsh_help , abvsh_exit };
+
+int executer(char **tokens){
+  if (tokens[0] == NULL){
+    return 1;
+  }
+  for(int i = 0 ; i < no_of_builtin() ; i++){
+    if(strcmp(tokens[0],builtin_func[i]) == 0 ){
+     return (*farr[i])(tokens);
+    }
+  }
+  return create(tokens);
 }
 
